41fafa8e4a19c2710eac7e9f7974f5c367c18e11
Do Trung Kien <trungkien.keio@gmail.com>
2018-08-08 14:38:53 +0700

    deploy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# drizzle-react-sample
+# Peer point sample with [Drizzle](https://truffleframework.com/docs/drizzle/getting-started)
+
+## Ropten testnet
+
+[peer-point-drizzle.surge.sh](peer-point-drizzle.surge.sh)
--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+var HDWalletProvider = require("truffle-hdwallet-provider")
+
+require('dotenv').config()
+var infura_apikey = process.env.INFURA_KEY
+var mnemonic = process.env.MNEMONIC
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -5,6 +11,18 @@ module.exports = {
       host: "localhost",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey, 2),
+      network_id: 3
+    },
+    kovan: {
+        provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey, 3),
+        network_id: 42
+    },
+    rinkeby: {
+        provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"+infura_apikey, 4),
+        network_id: 4,
     }
   },
   solc: {
@@ -12,5 +30,5 @@ module.exports = {
       enabled: true,
       runs: 500
     }
-  } 
-};
+  }
+}
